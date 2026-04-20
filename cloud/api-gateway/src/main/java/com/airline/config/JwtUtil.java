b78ac437d9dff72ec1aaae159d0516f534c070d5
Trung Nguyen <trung.nguyen@msn.com>
2026-04-20 18:33:40 +0700

    add initial implementation of API Gateway with routing and JWT authentication

---
+++ cloud/api-gateway/src/main/java/com/airline/config/JwtUtil.java
@@ -0,0 +1,56 @@
+package com.airline.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret-key}")
+    private String SECRET_KEY;
+
+    private final SecretKey key;
+
+    public JwtUtil() {
+        this.key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).get("email", String.class);
+    }
+
+    public String extractAuthorities(String token) {
+        return extractAllClaims(token).get("authorities", String.class);
+    }
+
+    public Long extractUserId(String token) {
+        return extractAllClaims(token).get("userId", Long.class);
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Duration getRemainingValidity(String token) {
+        Date expiration = extractAllClaims(token).getExpiration();
+        return Duration.between(Instant.now(), expiration.toInstant());
+    }
+}
